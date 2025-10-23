bf9aa993449047d0d542f78372e3856502d471a6
nahuel <nahuel.piazza@yahoo.com>
2025-10-23 13:59:58 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame as pg
+from configurations import display
+
+
+
+screen = pg.display.set_mode((display.WIDTH, display.HEIGHT))
+pg.display.set_caption("My Pygame Window")
+
+run = True
+while run:
+    for event in pg.event.get(): #entrega la lista de eventos que pueden ocurrir en el juego
+        if event.type == pg.QUIT: # si el tipo de evento coincide con el evento de salir
+            run = False # entonces salir del bucle
+
+
+pg.quit()
+    
